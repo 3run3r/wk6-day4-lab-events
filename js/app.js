4c1cd48072c9a3bf6d75a56c66b6ab42a4283bbd
Gary Clark <gclark04432@live.co.uk>
2020-01-09 15:11:45 +0000

    can add new item

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+
   console.log('JavaScript loaded');
-})
+});
+
+const handleFormSubmit = function (event) {
+  event.preventDefault();
+
+  const newItem = document.createElement('li');
+
+  const title = event.target.title.value;
+  const author = event.target.author.value;
+  const category = event.target.category.value;
+
+  newItem.textContent = `${title}, ${author}, ${category}`;
+
+  const unorderedList = document.querySelector('ul');
+  unorderedList.appendChild(newItem);
+};
